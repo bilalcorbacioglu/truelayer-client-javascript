cca64fc254297ad74d1c3eabd719fc0ee59a05ca
Bilal Corbacioglu <bilalcorbacioglu@gmail.com>
2020-01-18 15:44:05 +0000

    Auth and Api url changed for Sandbox

--- src/v1/Constants.ts
+++ src/v1/Constants.ts
@@ -4,10 +4,8 @@
  */
 export class Constants {
     // Constants
-    public static readonly AUTH_URL: string = "https://auth.truelayer.com";
-    public static readonly API_URL: string = "https://api.truelayer.com";
-    public static readonly AUTH_SANDBOX_URL: string = "https://auth.truelayer-sandbox.com";
-    public static readonly API_SANDBOX_URL: string = "https://api.truelayer-sandbox.com";
+    public static readonly AUTH_URL: string = "https://auth.truelayer-sandbox.com";
+    public static readonly API_URL: string = "https://api.truelayer-sandbox.com";
     public static readonly STATUS_URL: string = "https://status-api.truelayer.com";
     public static readonly API_TIMEOUT: number = 60000;
     public static readonly AVAILABLE_SCOPES: Array<string> = ["offline_access", "info", "accounts", "transactions", "balance", "cards", "direct_debits", "standing_orders"];
